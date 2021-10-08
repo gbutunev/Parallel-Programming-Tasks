e053c604245b35ab0afb2e5973211e4dfcba0248
Georgi Byutyunev <78198180+gbutunev@users.noreply.github.com>
2021-10-08 20:19:42 +0300

    Add project files.

---
+++ T1_Chitanka/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace T1_Chitanka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string runningDir = Environment.CurrentDirectory;
+            string bookDir = Directory.GetParent(runningDir).Parent.Parent.ToString();
+            string[] allLines = File.ReadAllLines(bookDir + "\\Orwell-1984.txt");
+            
+            List<string> allWords = new List<string>();
+            //HashSet<string> distinctWords = new HashSet<string>();
+
+            Regex regex = new Regex(@"\.|,|\?|!|„|“|:|;| — | - | |\t"); //т.е. & т.н.?
+            foreach (var line in allLines)
+            {
+                string input = line.ToLower();
+
+                string[] words = regex.Split(input);
+
+                foreach (var word in words)
+                {
+                    if (word.Trim() != string.Empty)
+                    {
+                        allWords.Add(word);
+                        //distinctWords.Add(word);
+                    }
+                }
+            }
+
+
+            long time1 = Tasks.RunSynchronous(allWords);
+            Console.WriteLine($"Time elapsed for synchronous tasks: {time1}\n\n\n");
+
+            long time2 = Tasks.RunParallel(allWords);
+            Console.WriteLine($"Time elapsed for parallel tasks: {time2}");
+
+        }
+    }
+}
---
+++ T1_Chitanka/Tasks.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace T1_Chitanka
+{
+    public static class Tasks
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static long RunSynchronous(List<string> words)
+        {
+            stopwatch.Restart();
+
+            PrintNumberOfWords(words);
+            PrintLongestWord(words);
+            PrintAverageWordLength(words);
+            PrintFiveMostCommonWords(words);
+            PrintFiveLeastCommonWords(words);
+
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        public static long RunParallel(List<string> words)
+        {
+            List<Thread> threads = new List<Thread>();
+            threads.Add(new Thread(PrintNumberOfWords));
+            threads.Add(new Thread(PrintLongestWord));
+            threads.Add(new Thread(PrintAverageWordLength));
+            threads.Add(new Thread(PrintFiveMostCommonWords));
+            threads.Add(new Thread(PrintFiveLeastCommonWords));
+
+            stopwatch.Restart();
+            foreach (var thread in threads) thread.Start(words);
+
+            foreach (var thread in threads) thread.Join();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private static void PrintFiveLeastCommonWords(object list)
+        {
+            List<string> words = (List<string>)list;
+
+            string[] top5 = words.GroupBy(w => w)
+                            .OrderBy(gr => gr.Count())
+                            .Take(5)
+                            .Select(gr => gr.Key)
+                            .ToArray();
+
+            string output = "Top 5 least common words:\n";
+            for (int i = 0; i < 5; i++)
+            {
+                output += $"{i + 1}. {top5[i]}\n";
+            }
+
+            Console.WriteLine(output);
+        }
+
+        private static void PrintFiveMostCommonWords(object list)
+        {
+            List<string> words = (List<string>)list;
+
+            string[] top5 = words.GroupBy(w => w)
+                                        .OrderByDescending(gr => gr.Count())
+                                        .Take(5)
+                                        .Select(gr => gr.Key)
+                                        .ToArray();
+
+            string output = "Top 5 most common words:\n";
+            for (int i = 0; i < 5; i++)
+            {
+                output += $"{i + 1}. {top5[i]}\n";
+            }
+
+            Console.WriteLine(output);
+        }
+
+        private static void PrintAverageWordLength(object list)
+        {
+            List<string> words = (List<string>)list;
+
+            double length = words.Average(w => w.Length);
+            Console.WriteLine($"Average word length is {length:F2}");
+        }
+
+        private static void PrintLongestWord(object list)
+        {
+            List<string> words = (List<string>)list;
+
+            int currLength = 0;
+            string currWord = string.Empty;
+
+            foreach (string word in words)
+            {
+                if (word.Length > currLength)
+                {
+                    currLength = word.Length;
+                    currWord = word;
+                }
+            }
+
+            Console.WriteLine($"Longest word is \"{currWord}\" with {currLength} characters");
+        }
+
+        private static void PrintNumberOfWords(object list)
+        {
+            List<string> words = (List<string>)list;
+
+            Console.WriteLine($"Total number of words: {words.Count}");
+        }
+    }
+}
